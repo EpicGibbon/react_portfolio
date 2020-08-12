e65685d56ba66aa868e46f4440338c06fb81082d
EpicGibbon <michant1997@gmail.com>
2020-08-12 18:20:18 -0400

    routes done, header needs to be compontized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import Footer from '../src/Components/Footer'
+import HomePage from '../src/pages/HomePage'
+import AboutPage from '../src/pages/AboutPage'
+import ContactPage from '../src/pages/ContactPage'
 
 class App extends React.Component {
 
@@ -39,18 +42,34 @@ class App extends React.Component {
           <Navbar className="border-bottom" bg="transparent" expand="md">
             <Navbar.Brand>Anthony Aguiar</Navbar.Brand>
 
-            <Navbar.Toggle className="border-bottom"aria-controls="navbar-toggle" />
+            <Navbar.Toggle className="border-bottom" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
+
+
               </Nav>
             </Navbar.Collapse>
           </Navbar>
 
+          <Route path="/" exact render={() => <HomePage
+            title={this.state.home.title}
+            subTitle={this.state.home.subTitle}
+            text={this.state.home.text}
+          />} />
+
+          <Route path="/about" exact render={() => <AboutPage
+            title={this.state.about.title}
+          />} />
+
+          <Route path="/contact" exact render={() => <ContactPage
+            title={this.state.contact.title}
+          />} />
 
           <Footer />
+
         </Container>
       </Router>
     );
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+
+    return (
+        <p>Home Works!</p>
+    )
+}
+
+
+export default Home;
