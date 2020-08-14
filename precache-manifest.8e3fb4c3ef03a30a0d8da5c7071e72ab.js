c2842a61e062c2290c1aa411c5b58e30d9d9df9f
EpicGibbon <michant1997@gmail.com>
2020-08-13 22:10:31 -0400

    Updates

---
+++ precache-manifest.8e3fb4c3ef03a30a0d8da5c7071e72ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea57617d73cca954db4cf6a4acce9b9e",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "04533e25b6e1d341e8ce",
+    "url": "/react_portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "dc020077af4fe03d0ee1",
+    "url": "/react_portfolio/static/css/main.d230e7d6.chunk.css"
+  },
+  {
+    "revision": "04533e25b6e1d341e8ce",
+    "url": "/react_portfolio/static/js/2.1b18eaf9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_portfolio/static/js/2.1b18eaf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc020077af4fe03d0ee1",
+    "url": "/react_portfolio/static/js/main.c1a716b1.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "53177410add1178720b42261af9a6433",
+    "url": "/react_portfolio/static/media/pbudget.53177410.png"
+  },
+  {
+    "revision": "189a1c717a2dc51ab80be278b47c9b23",
+    "url": "/react_portfolio/static/media/workout.189a1c71.png"
+  },
+  {
+    "revision": "f2251c7e0647515c2974dda9a907dc60",
+    "url": "/react_portfolio/static/media/xfiles.f2251c7e.jpg"
+  }
+]);
